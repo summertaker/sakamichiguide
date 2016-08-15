13eff9e548c3346d6a2c20f793dca9e5bcfaef6e
summertaker <dfsfer34>
2016-08-15 13:06:40 +0900

    v3.5

--- app/src/main/java/com/summertaker/sakamichiguide/common/Config.java
+++ app/src/main/java/com/summertaker/sakamichiguide/common/Config.java
@@ -2,7 +2,7 @@ package com.summertaker.sakamichiguide.common;
 
 public class Config {
     public final static String PLATFORM = "android-phone";
-    public final static String VERSION = "3.3";
+    public final static String VERSION = "3.5";
 
     public final static String PACKAGE_NAME = "com.summertaker.sakamichiguide";
     public final static String USER_PREFERENCE_KEY = PACKAGE_NAME;
